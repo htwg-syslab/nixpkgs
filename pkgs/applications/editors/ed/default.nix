htwg-syslab/nixpkgs

commit 7bdacad8b39568611e84da5aa3b37e4e70641012

ed: Modernize and fix for cross

pkgs/applications/editors/ed/default.nix
@@ -1,4 +1,6 @@
-{ fetchurl, stdenv }:
+{ stdenv, fetchurl
+, buildPlatform, hostPlatform
+}:
 
 stdenv.mkDerivation rec {
   name = "ed-${version}";
@@ -28,11 +30,12 @@ stdenv.mkDerivation rec {
        make: *** [check] Error 127
 
     */
-  doCheck = !stdenv.isDarwin;
+  doCheck = !(hostPlatform.isDarwin || hostPlatform != buildPlatform);
 
-  crossAttrs = {
-    compileFlags = [ "CC=${stdenv.cross.config}-gcc" ];
-  };
+  configureFlags = if hostPlatform == buildPlatform then null else [
+    "--exec-prefix=${stdenv.cc.prefix}"
+    "CC=${stdenv.cc.prefix}cc"
+  ];
 
   meta = {
     description = "An implementation of the standard Unix editor";

commit 865e6714a11763404056830d13bf47544f263fab

ed: Fix executable name
7bdacad8b39568611e84da5aa3b37e4e70641012 broke the `ed` derivation for
cross-compilation, making it install ed with,

    install -m 755 ./ed "/nix/store/$hash-ed-1.14.2x86_64-unknown-linux-gnu-/bin/ed"

Clearly this is nonsense.

pkgs/applications/editors/ed/default.nix
@@ -11,7 +11,7 @@ stdenv.mkDerivation rec {
     sha256 = "1nqhk3n1s1p77g2bjnj55acicsrlyb2yasqxqwpx0w0djfx64ygm";
   };
 
-  unpackCmd = "tar --lzip -xf";
+  unpackCmd = "tar --lzip -xf $curSrc";
 
   nativeBuildInputs = [ lzip ];
 
@@ -26,10 +26,7 @@ stdenv.mkDerivation rec {
     */
   doCheck = !(hostPlatform.isDarwin || hostPlatform != buildPlatform);
 
-  installFlags = [ "DESTDIR=$(out)" ];
-
   configureFlags = [
-    "--exec-prefix=${stdenv.cc.prefix}"
     "CC=${stdenv.cc.prefix}cc"
   ];
 